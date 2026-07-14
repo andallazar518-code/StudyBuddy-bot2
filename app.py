6357f28e65e9cf324b73456af0ebeb50509d125b
andallazar518-code <andallazar518@gmail.com>
2026-07-14 21:02:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ PRODUCT_MAP = {
 }
 
 genai.configure(api_key=GOOGLE_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-8b-latest')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 MAX_TURNS = 6
 chat_sessions = defaultdict(lambda: deque(maxlen=MAX_TURNS))
